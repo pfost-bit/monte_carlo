e91d73ef7a125f1d388e3d73431f0179d2590f6f
Patrick Foster <ezq9qu@virginia.edu>
2024-11-06 14:14:50 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='monte_carlo',
+    version='0.1',
+    description='A montecarlo simulator written for DS5100',
+    url='https://github.com/pfost-bit/monte_carlo',
+    author='Pat Foster',
+    author_email='ezq9qu@virginia.edu',
+    license='MIT',
+    packages=["monte_carlo"]
+)
