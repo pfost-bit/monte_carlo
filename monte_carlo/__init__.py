c51ea0b68e666598dd7acc5a926ec632556d9310
pfost-bit <ezq9qu@virginia.edu>
2024-11-06 14:52:33 -0500

    Changes to the __init__

--- monte_carlo/__init__.py
+++ monte_carlo/__init__.py
@@ -1,5 +1,2 @@
-from monte_carlo.analyzer import Analyzer as Analyzer
-from monte_carlo.die import Die as Die
-from monte_carlo.game import Game as Game
-import pandas as pd
-import numpy as np
+from . import analyzer, die, game
+
--- monte_carlo/game.py
+++ monte_carlo/game.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+from die import Die
 
 class Game():
     """
